bed79fcc380196035241a54ff19ba234266ef8b0
Ian Avelar <ian.peixoto@pisomtech.com.br>
2020-04-18 13:13:08 +0200

    Added some tests with jest

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,5 +1,6 @@
 const connection = require('../database/connection');
 const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 
 module.exports = {
     async list (request, response) {
@@ -11,7 +12,7 @@ module.exports = {
     async create(request, response) {
         const { name, email, whatsapp, city, uf } = request.body; // Garantir que o usuario mande as informacoes necessarias e apenas elas.
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
